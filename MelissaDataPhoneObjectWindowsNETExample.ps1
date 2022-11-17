759cb483c3b3053d418441fd7687598b0fa21f75
Melissa <erik@melissadata.com>
2022-11-17 10:43:16 -0800

    Tested successfully using Melissa data files for 2022.10

--- MelissaDataPhoneObjectWindowsNETExample.ps1
+++ MelissaDataPhoneObjectWindowsNETExample.ps1
@@ -19,7 +19,7 @@ class DLLConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2022.11'
+$RELEASE_VERSION = '2022.10'
 $ProductName = "DQ_PHONE_DATA"
 
 # Uses the location of the .ps1 file 
